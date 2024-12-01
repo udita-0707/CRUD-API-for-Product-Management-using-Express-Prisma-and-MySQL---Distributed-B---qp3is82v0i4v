803d09ae482744f71e4df5c50dac8fe0ece2aaf6
Udita <153092248+udita-0707@users.noreply.github.com>
2024-12-02 04:09:07 +0530

    done

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  partialUpdateProduct,
+  deleteProduct,
+} = require('../controllers/productController');
+const authMiddleware = require('../middleware/authMiddleware');
+
 const router = express.Router();
 
-//write your code here
+// Apply the authMiddleware to all routes
+router.use(authMiddleware);
+
+// Routes
+router.post('/create', createProduct);
+router.get('/get', getAllProducts);
+router.get('/getById/:id', getProductById);
+router.put('/put/:id', updateProduct);
+router.patch('/patch/:id', partialUpdateProduct);
+router.delete('/delete/:id', deleteProduct);
 
 module.exports = router;
